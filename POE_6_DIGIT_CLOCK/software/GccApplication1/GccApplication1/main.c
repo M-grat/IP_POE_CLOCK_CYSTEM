fd82bd57cf12b6790d9f6c38fb01123af93b5d8c
Muhammet Çiçek <66729825+M-grat@users.noreply.github.com>
2022-03-19 17:12:41 +0300

    Muhammet ÇİÇEK

---
+++ POE_6_DIGIT_CLOCK/software/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 19/03/2022 5:12:08 pm
+ * Author : muham
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
